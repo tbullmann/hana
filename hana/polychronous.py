f0407255651a4407632eaad12363bbea79150cd4
tbullmann <bullmann.torsten@gmail.com>
2016-11-14 21:26:20 +0900

    Removed nonsense code in polycronous.filter

--- hana/polychronous.py
+++ hana/polychronous.py
@@ -1,9 +1,6 @@
-from itertools import product, groupby
 import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
-
-from hana.misc import unique_neurons
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -23,31 +20,29 @@ def filter(timeseries, axonal_delays, synaptic_delay=0.001, jitter=0.001):
     # TODO: Improve function description, remove unit inconsistencies (ms vs. s)
     # Note: for testing: 9861 -> 10964 with predicted spike time lag 1.266667 ms: 93(candidate) pairs
     connected_events = []
-    neuron_set = unique_neurons(axonal_delays)
-    for pre, post in product(neuron_set, repeat=2):
-        if (pre, post) in axonal_delays:
-            time_lag = (axonal_delays[pre, post])/1000 + synaptic_delay  # axonal delays in ms -> events in s
-            logging.info("Finding spike pairs %d -> %d with predicted spike time lag %f ms:" % (pre, post, time_lag))
+    for pre, post in axonal_delays:
+        time_lag = (axonal_delays[pre, post])/1000 + synaptic_delay  # axonal delays in ms -> events in s
+        logging.info("Finding spike pairs %d -> %d with predicted spike time lag %f ms:" % (pre, post, time_lag))
 
-            _pre = pre-1     # Indicies for neurons in events starting at 0, in delays from 1 (!)
-            _post = post-1   # TODO: Fix this BIG BUG!
+        _pre = pre-1     # Indicies for neurons in events starting at 0, in delays from 1 (!)
+        _post = post-1   # TODO: Fix this BIG BUG!
 
-            if (_pre in timeseries) and (_post in timeseries):
-                presynaptic_spikes = timeseries[_pre]
-                shifted_presynaptic_spikes = presynaptic_spikes+time_lag
-                postsynaptic_spikes = timeseries[_post]
-                for offset in (0, 1):  # checking postsynaptic spike before and after shifted presynaptic spike
-                    position = (np.searchsorted(postsynaptic_spikes, shifted_presynaptic_spikes) - offset).\
-                        clip(0, len(postsynaptic_spikes) - 1)
-                    valid = np.abs(shifted_presynaptic_spikes - postsynaptic_spikes[position]) < jitter
-                    valid_presynaptic_spikes = presynaptic_spikes[valid]
-                    valid_postsynaptic_spikes = postsynaptic_spikes[position[valid]]
-                    new_connected_events = [((pre_time, pre), (post_time, post))
-                                            for pre_time, post_time
-                                            in (zip(valid_presynaptic_spikes, valid_postsynaptic_spikes))]
-                    logging.info("From %d candidates add %d valid to %d existing pairs"
-                                 % (len(position), len(new_connected_events), len(connected_events)))
-                    connected_events = connected_events + new_connected_events
+        if (_pre in timeseries) and (_post in timeseries):
+            presynaptic_spikes = timeseries[_pre]
+            shifted_presynaptic_spikes = presynaptic_spikes+time_lag
+            postsynaptic_spikes = timeseries[_post]
+            for offset in (0, 1):  # checking postsynaptic spike before and after shifted presynaptic spike
+                position = (np.searchsorted(postsynaptic_spikes, shifted_presynaptic_spikes) - offset).\
+                    clip(0, len(postsynaptic_spikes) - 1)
+                valid = np.abs(shifted_presynaptic_spikes - postsynaptic_spikes[position]) < jitter
+                valid_presynaptic_spikes = presynaptic_spikes[valid]
+                valid_postsynaptic_spikes = postsynaptic_spikes[position[valid]]
+                new_connected_events = [((pre_time, pre), (post_time, post))
+                                        for pre_time, post_time
+                                        in (zip(valid_presynaptic_spikes, valid_postsynaptic_spikes))]
+                logging.info("From %d candidates add %d valid to %d existing pairs"
+                             % (len(position), len(new_connected_events), len(connected_events)))
+                connected_events = connected_events + new_connected_events
 
     logging.info("Total %d pairs" % len(connected_events))
     return connected_events
