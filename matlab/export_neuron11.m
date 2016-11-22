807ac8b7b14134e468c882d98a1cad7649d0cd19
tbullmann <bullmann.torsten@gmail.com>
2016-11-22 10:55:38 +0900

    Initital commit

---
+++ matlab/export_neuron11.m
@@ -0,0 +1,22 @@
+%TODO: Fix the BIG BUG in the matlab export script / Neuron2Electrode: Indicies for electrodes in events
+%correctly start at 0, in delays and positive peak the (trigger) electrodes from 1 (!)
+
+% Import
+neurons=events.neurons;
+NeuronTable = events.NeuronTable;
+clear events;
+
+
+neuron_index = 11;
+x = neurons{neuron_index}.x;
+y = neurons{neuron_index}.y;
+distances = distn([x' y']',[x' y']');
+V = neurons{neuron_index}.mean ;trigger_el_idx = Neuron2Electrode ( NeuronTable, neuron_index);
+pre = 80;
+post = 80;
+sampling_frequency = 20000;
+time = (-pre:post)/sampling_frequency;
+
+
+filename = '/Users/tbullmann/Desktop/neuron11.h5';
+hdf5write(filename, '/V',V, '/time',time/1000, '/x',x, '/y',y , '/trigger',trigger_el_idx-1, '/neuron',neuron_index)
