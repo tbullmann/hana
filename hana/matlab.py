84893a99876d1ce22fddf5e7a8806eabe9c02a1a
tbullmann <bullmann.torsten@gmail.com>
2016-11-22 10:39:34 +0900

    First commit for load_traces (from hdf5 file), working.

--- hana/matlab.py
+++ hana/matlab.py
@@ -1,3 +1,4 @@
+import h5py
 import numpy as np
 import scipy.io as sio
 import logging
@@ -49,3 +50,25 @@ def load_neurites(filename):
     return delay, positive_peak
 
 
+def load_traces(filename):
+    """
+
+    :param filename: path and name of the hdf5 file
+    :return: V: recordings as an array with the shape electrodes x time
+    :return: t: time in s, or None
+    :return: x, y: coordinates of the recording electrodes
+    :return: trigger, neuron: index of recording electrode and corresponding neuron, if spike triggered averaging was
+    performed otherwise None
+    """
+    file = h5py.File(filename, 'r')
+    V = get_variable(file, 'V')
+    t = get_variable(file, 't')
+    x = get_variable(file, 'x')
+    y = get_variable(file, 'y')
+    trigger = get_variable(file, 'trigger')
+    neuron = get_variable(file, 'neuron')
+    logging.info ('Load file %s with variables %s' % (filename, file.keys()))
+    return V, t, x, y, trigger, neuron
+
+
+def get_variable(file, key): return np.array(file[key] if key in file.keys() else None)
