4e333b66355a4b9cb1952b613b7970e94b8e013e
tbullmann <bullmann.torsten@gmail.com>
2016-12-07 17:40:16 +0900

    Renamed thr_overlap to thr_ratio reflecting it's use, but added a proper thr_overlap instead

--- hana/structure.py
+++ hana/structure.py
@@ -4,7 +4,7 @@ from itertools import product
 import numpy as np
 
 
-def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak=10, thr_overlap=0.10):
+def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak=10, thr_ratio=0.10, thr_overlap=0):
     delay = np.nan
     overlap_ratio = np.nan
     axon_field = np.greater(axon_delay[presynaptic_neuron], 0)
@@ -14,15 +14,15 @@ def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neu
     if dendrite_size > 0:
         overlap_size = sum(overlap)
         overlap_ratio = float(overlap_size) / dendrite_size
-        if thr_overlap < overlap_ratio:
+        if thr_ratio < overlap_ratio and thr_overlap < overlap_size:
             delay = np.mean(axon_delay[presynaptic_neuron][overlap])
-            logging.debug('overlap = %1.2f with mean delay = %1.1f [ms]' % (overlap_ratio, delay))
+            logging.debug('overlap at %d electrodes (ratio= %1.2f) with mean delay = %1.1f [ms]' % (overlap_size, overlap_ratio, delay))
         else:
-            logging.debug('overlap = %1.2f too small, no delay assigned' % overlap_ratio)
+            logging.debug('overlap at %d electrodes (ratio= %1.2f) too small, no delay assigned' % (overlap_size, overlap_ratio) )
     return overlap, overlap_ratio, delay
 
 
-def all_overlaps (axon_delay, dendrite_peak, thr_peak=10, thr_overlap=0.10):
+def all_overlaps(axon_delay, dendrite_peak, thr_peak=10, thr_ratio=0.10, thr_overlap=0):
     """Compute overlaps"""
     all_overlap, all_overlap_ratios, all_axonal_delays = [], [], []
     for pair in product(axon_delay, repeat=2):
@@ -30,7 +30,7 @@ def all_overlaps (axon_delay, dendrite_peak, thr_peak=10, thr_overlap=0.10):
         if presynaptic_neuron<>postsynaptic_neuron:
             logging.debug('neuron %d -> neuron %d:' % pair)
             overlap, ratio, delay = find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron,
-                                        thr_peak=thr_peak, thr_overlap=thr_overlap)
+                                                 thr_peak=thr_peak, thr_ratio=thr_ratio, thr_overlap=thr_overlap)
             if np.isfinite(delay):
                 all_overlap.append((pair, overlap))
                 all_overlap_ratios.append((pair, ratio))
