0c318fa726dfa07f1de6a6d960c73f69739db821
tbullmann <bullmann.torsten@gmail.com>
2016-12-13 19:56:31 +0900

    Added documentation

--- hana/structure.py
+++ hana/structure.py
@@ -5,6 +5,17 @@ import numpy as np
 
 
 def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak=0, thr_ratio=0.0, thr_overlap=1):
+    """
+    Compute the overlap between a pre- and a post-synaptic neuron.
+    :param axon_delay: dictionary of axonal fields indexed for the pre-synaptic neuron
+    :param dendrite_peak: dictionary of dendritic fields indexed the post-synaptic neuron
+    :param presynaptic_neuron: index of the presynaptic neuron
+    :param postsynaptic_neuron: index of the postsynaptic neuron
+    :param thr_peak: threshold for the positive peak (return current) in uV
+    :param thr_ratio: threshold for the overlap relative to the dendritic field
+    :param thr_overlap: threshold for the overlap of axonal and dendritic field (in number of electrodes)
+    :return: Size, ratio and delay if the neurons are connected, otherwise NaN.
+    """
     delay = np.nan
     overlap_size = np.nan
     overlap_ratio = np.nan
@@ -24,7 +35,15 @@ def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neu
 
 
 def all_overlaps(axon_delay, dendrite_peak, thr_peak=0, thr_ratio=0, thr_overlap=1):
-    """Compute overlaps"""
+    """
+    Compute all pairwise overlaps.
+    :param axon_delay: dictionary of axonal fields indexed for neuron
+    :param dendrite_peak: dictionary of dendritic fields indexed neuron
+   :param thr_peak: threshold for the positive peak (return current) in uV
+    :param thr_ratio: threshold for the overlap relative to the dendritic field
+    :param thr_overlap: threshold for the overlap of axonal and dendritic field (in number of electrodes)
+    :return: dictionary of overlap, overlap ratio, and delays indexed by (connected) neuron pair
+    """
     all_overlap, all_overlap_ratios, all_axonal_delays = [], [], []
     for pair in product(axon_delay, repeat=2):
         presynaptic_neuron, postsynaptic_neuron = pair
