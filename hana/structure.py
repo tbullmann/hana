7996cb1d5a7523edadf6aaf2712c82ae8cff23d0
tbullmann <bullmann.torsten@gmail.com>
2016-12-13 19:51:09 +0900

    Default threshold for dendritic positive peak, thr_peak=0, for overlap between axons and dendrites, thr_overlap=1, and their ratio, thr_ratio=0.

--- hana/structure.py
+++ hana/structure.py
@@ -4,8 +4,9 @@ from itertools import product
 import numpy as np
 
 
-def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak=10, thr_ratio=0.10, thr_overlap=0):
+def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak=0, thr_ratio=0.0, thr_overlap=1):
     delay = np.nan
+    overlap_size = np.nan
     overlap_ratio = np.nan
     axon_field = np.greater(axon_delay[presynaptic_neuron], 0)
     dendritic_field = np.greater(dendrite_peak[postsynaptic_neuron], thr_peak)
@@ -22,12 +23,12 @@ def find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neu
     return overlap_size, overlap_ratio, delay
 
 
-def all_overlaps(axon_delay, dendrite_peak, thr_peak=10, thr_ratio=0.10, thr_overlap=0):
+def all_overlaps(axon_delay, dendrite_peak, thr_peak=0, thr_ratio=0, thr_overlap=1):
     """Compute overlaps"""
     all_overlap, all_overlap_ratios, all_axonal_delays = [], [], []
     for pair in product(axon_delay, repeat=2):
         presynaptic_neuron, postsynaptic_neuron = pair
-        if presynaptic_neuron<>postsynaptic_neuron:
+        if presynaptic_neuron != postsynaptic_neuron:
             logging.debug('neuron %d -> neuron %d:' % pair)
             overlap, ratio, delay = find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron,
                                                  thr_peak=thr_peak, thr_ratio=thr_ratio, thr_overlap=thr_overlap)
